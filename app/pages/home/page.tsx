9023ce89b1a6fcbb6af392ecdebeeb5faa4faa99
beatitude200 <beatitude102@gmail.com>
2024-08-10 06:31:00 +0100

    first commit

---
+++ app/pages/home/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is rendered from the `app/page.tsx` file.</p>
+    </div>
+  );
+}
