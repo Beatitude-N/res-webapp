6453d345e42cc05e4ba5717ff4543da3df4e6899
beatitude200 <beatitude102@gmail.com>
2024-08-12 09:00:53 +0100

    last comit

---
+++ app/api/call/route.js
@@ -0,0 +1,17 @@
+// export default function handler(req, res) {
+//     if (req.method === 'GET') {
+//       res.status(200).json({ message: 'Hello, world!' });
+//     } else {
+//       res.status(405).json({ error: 'Method not allowed' });
+//     }
+//   }
+  
+export async function GET(request) {
+  return new Response(JSON.stringify({ message: 'Hello, world!' }), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+  
+  
