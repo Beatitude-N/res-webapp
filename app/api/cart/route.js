91e84d81cfa704160b855b48f388fb68a9639f25
beatitude200 <beatitude102@gmail.com>
2024-09-08 23:15:24 +0100

    last

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -1,25 +1,6 @@
-// app/api/postToFoxyCart/route.js
-export async function POST(req) {
-    const url = "https://wizhom.foxycart.com/cart"; 
-    const cartItems = await req.json();
-
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(cartItems)
-        });
-
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-        }
-
-        const result = await response.json();
-        return new Response(JSON.stringify(result), { status: 200 });
-    } catch (error) {
-        console.error("Error posting to FoxyCart:", error);
-        return new Response(JSON.stringify({ error: "Error posting to FoxyCart" }), { status: 500 });
-    }
-}
+export async function GET(request) {
+    return new Response(JSON.stringify({ message: 'Hello, world!' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
